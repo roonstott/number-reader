6384ba5e53cab2b61f164a1c06c1cb39b2af71f5
Robert Onstott <roonstott@gmail.com>
2022-12-08 13:53:05 -0800

    Test Pass: Number constructor creates instance of class with UserNumber field
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- NumberReader.Tests/ModelTests/NumberReaderTests.cs
+++ NumberReader.Tests/ModelTests/NumberReaderTests.cs
@@ -11,17 +11,18 @@ namespace NumberReader.Tests
     [TestMethod]
     public void Number_CreateInstanceOfNumber_Number()
     {
-      Number newNumber = new Number();
+      Number newNumber = new Number("7");
       Assert.AreEqual(typeof(Number), newNumber.GetType());
     }
 
     [TestMethod]
     public void GatherUserInput_ReturnsNumber_Int()
     {
+      Number newNumber = new Number("7");
       string stringUserNumber = "7";
-      int userNumber = Int32.Parse(stringUserNumber);
+      int expected = Int32.Parse(stringUserNumber);
       int result = newNumber.UserNumber;
-      Assert.AreEqual(userNumber, result);
+      Assert.AreEqual(expected, result);
     }
   }
 }
--- NumberReader/Models/NumberReader.cs
+++ NumberReader/Models/NumberReader.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 
 namespace NumberReader.Models
 {
+
   public class Number
   {
-    // properties, constructors, methods, etc. go here
+    public int UserNumber { get; set; }
+    public Number(string stringUserNumber)
+    {
+      UserNumber = Int32.Parse(stringUserNumber);
+    }
   }
 }
