7e69318b28b3959fe9c07e997b5c5d169f67514c
Robert Onstott <roonstott@gmail.com>
2022-12-08 13:41:59 -0800

    Test: should return UserNumber field of newNumber instance of Number class
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- NumberReader.Tests/ModelTests/NumberReaderTests.cs
+++ NumberReader.Tests/ModelTests/NumberReaderTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NumberReader.Models;
+using System;
+using System.Collections.Generic;
 
 namespace NumberReader.Tests
 {
@@ -7,11 +9,19 @@ namespace NumberReader.Tests
   public class NumberReaderTests
   {
     [TestMethod]
-
     public void Number_CreateInstanceOfNumber_Number()
     {
       Number newNumber = new Number();
       Assert.AreEqual(typeof(Number), newNumber.GetType());
+    }
+
+    [TestMethod]
+    public void GatherUserInput_ReturnsNumber_Int()
+    {
+      string stringUserNumber = "7";
+      int userNumber = Int32.Parse(stringUserNumber);
+      int result = newNumber.UserNumber;
+      Assert.AreEqual(userNumber, result);
     }
   }
 }
